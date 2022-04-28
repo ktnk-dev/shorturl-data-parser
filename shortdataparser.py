d6bae38149df0f7ff8589058889abd934eb70337
ktnk ♡ <63897347+ktnk-dev@users.noreply.github.com>
2022-04-28 08:50:19 +0300

    багфикс

--- shortdataparser.py
+++ shortdataparser.py
@@ -9,5 +9,5 @@ def get(id):
         return str("".join(url))   # получаем данные в виде текста
     
     except Exception as e:
-        if "ConnectionError" in str(e) and "nodename nor servname provided, or not known" in str(e):  # если домен, который мы сократили, НЕ существует
+        if "ConnectionError" in str(e):  # если домен, который мы сократили, НЕ существует
             return str(e).split(" url: ")[1].split(" (Caused")[0][1:] # все также возращаем пользовательские данные
